295a24a3dea80f43f586580a55a2318e61bb4abe
Tuan Phan <tphan@seattleu.edu>
2021-01-24 15:10:56 -0800

    Updated the value in ItemsPage

--- Mine/Mine/Services/MockDataStore.cs
+++ Mine/Mine/Services/MockDataStore.cs
@@ -15,7 +15,7 @@ namespace Mine.Services
             items = new List<ItemModel>()
             {
                 new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Ocular Device", Description="A glass to help the wearer see the hidden symbols.", Value=3 },
-                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "The Meerschaum Pipe", Description="A million-dollar pipe." },
+                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "The Meerschaum Pipe", Description="A million-dollar pipe.", Value=4 },
                 new ItemModel { Id = Guid.NewGuid().ToString(), Text = "President’s book", Description="A book holds all the secrets to the history of the World.", Value=5 },
                 new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Flaming gun", Description="A magical gun that can cause enemies to explode with solar energy.", Value=7 },
                 new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Cape of the captain", Description="A marvelous cape with the ability to reduce the damage of the enemies.", Value=9 },
