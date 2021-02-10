b6f1e0b4891322749951c7d7d1fb5f9f5c75d799
Tuan Phan <tphan@seattleu.edu>
2021-02-10 11:51:50 -0800

    Updated UT to use new ItemModel in ItemModelTests.cs

--- UnitTests/Models/ItemModelTests.cs
+++ UnitTests/Models/ItemModelTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using Mine.Models;
+using NUnit.Framework;
 
 namespace UnitTests.Models
 {
@@ -11,11 +12,12 @@ namespace UnitTests.Models
             // Arrange
 
             // Act
+            var result = new ItemModel();
 
             // Reset
 
             // Assert 
-            Assert.Pass();
+            Assert.IsNotNull(result);
         }
     }
 }
